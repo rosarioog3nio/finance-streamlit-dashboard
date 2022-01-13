bec443245db59ed3d3fff3e9831a277ba1e5ec12
Rosario de Sousa Fondo <rosarioogenio@gmail.com>
2022-01-13 15:12:12 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,9 +3,8 @@ import pandas as pd
 import numpy as np
 import requests
 import tweepy
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import time
-import requests
 
 
 st.image('PFF_Logo_WP.png')
